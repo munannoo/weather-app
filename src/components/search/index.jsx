107f6c7b9c4cd6fadb671b2991f29fd7f2d337b3
Manish Bagale <manish.bagale3@gmail.com>
2024-08-13 23:26:50 +0545

    corrected a typo

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Search() {
-  const [serach, setSearch] = useState(null);
+  const [search, setSearch] = useState(null);
   return (
     <div className="search-engine">
       Search
