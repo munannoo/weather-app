4483956173db22b1aeb5898305ca45cc508575a4
Manish Bagale <manish.bagale3@gmail.com>
2024-08-21 23:24:33 +0545

    added some styles

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -6,13 +6,16 @@ export default function Search({ search, setSearch, handleSearch }) {
       <h3 className="font-bold mb-2">Search for the city</h3>
       <input
         type="text"
-        className="p-3 py-2 rounded-lg mr-2"
+        className="p-3 py-2 rounded-lg mr-2 bg-lime-950"
         placeholder="Enter a city name"
         name="search"
         value={search}
         onChange={(event) => setSearch(event.target.value)}
       />
-      <button onClick={handleSearch} className="p-3 py-2 rounded-lg">
+      <button
+        onClick={handleSearch}
+        className="p-3 py-2 rounded-lg bg-lime-950"
+      >
         Submit
       </button>
     </div>
--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -37,6 +37,15 @@ export default function Weather() {
     return new Date();
   }
 
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
+
   return (
     <div className="bg-lime-700 p-4 rounded-lg">
       <h1 className="font-bold mb-4">Weather App</h1>
@@ -49,22 +58,35 @@ export default function Weather() {
 
       <div>
         {weatherData ? (
-          <div>
-            Results for <span className="font-bold">{weatherData.name}</span>
-            <p className="date">
-              {getCurrentDate().getDate() +
-                "/" +
-                getCurrentDate().getMonth() +
-                "/" +
-                getCurrentDate().getFullYear()}
-            </p>
-            <div className="weather-details">
-              <p className="description">
-                {weatherData?.weather[0]?.description}
+          <div className="mt-3 border-t-2 border-lime-600">
+            <div className="mt-3 location-details flex gap-5">
+              <p>
+                Results for{" "}
+                <span className="font-bold">{weatherData.name}</span>
               </p>
-              <p className="temp">{weatherData?.main?.temp}</p>
-              <p className="win">{weatherData?.wind?.speed}</p>
-              <p className="humidity">{weatherData?.main?.humidity}</p>
+              <p className="date">
+                {days[getCurrentDate().getDay()] +
+                  " / " +
+                  getCurrentDate().getHours() +
+                  ":00"}
+              </p>
+            </div>
+
+            <div className="weather-details text-center mt-3">
+              <div className="description mb-3">
+                <h1 className="temp">
+                  {Math.floor(weatherData?.main?.temp - 273.15)}
+                </h1>
+                <p>{weatherData?.weather[0]?.description}</p>
+              </div>
+              <div className="others flex flex-wrap justify-center mb-3">
+                <h3 className="basis-1/3">Wind</h3>
+                <h3 className="basis-1/3">Humidity</h3>
+                <p className="wind basis-1/3">{weatherData?.wind?.speed}</p>
+                <p className="humidity basis-1/3">
+                  {weatherData?.main?.humidity}
+                </p>
+              </div>
             </div>
           </div>
         ) : null}
